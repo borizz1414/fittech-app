8c3084e1a9b672c59030b03e39344e86bbfb0c6d
Boris <lopez98boris@gmail.com>
2021-05-03 18:01:18 -0400

    deploy boris

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   //url:'http://localhost:8000/api'
   //url:'http://192.168.0.103:8000/api'
   // url:'http://valdusoft.com/fittech/api'
-  url:'http://fittech247.com/fittech/api'
+  url:'https://smartbunnypruebas.com'
 
 
 };
